f3f091bbab377430eb7b01622d56445f64634837
Karen Manley <Karen Manley>
2017-07-15 15:10:55 -0400

    basic flash card generator completed

--- CLI.js
+++ CLI.js
@@ -1,15 +1,25 @@
 var BasicCard = require("./BasicCard");
 
 // Hold flashcard front - question
-var flashCardFront = process.argv[2];
+// var flashCardFront = process.argv[2];
     //console.log("Front of flashcard:  " + flashCardFront);
 
 // Hold flashcard back - answer
-var flashCardBack = process.argv[3];
+// var flashCardBack = process.argv[3];
     //console.log("Back of flashcard: " + flashCardBack);
 
 // Create a new flash card using the FlashCard object constructor
-var myFlashCard = new BasicCard(flashCardFront, flashCardBack);
+//var myFlashCard = new BasicCard(flashCardFront, flashCardBack);
 
-console.log("myFlashCard front :" + myFlashCard.front);
-console.log("myFlashCard back :" + myFlashCard.back);
+// console.log("myFlashCard front :" + myFlashCard.front);
+// console.log("myFlashCard back :" + myFlashCard.back);
+
+var firstPresident = new BasicCard(
+    "Who was the first president of the United States?", "George Washington");
+
+console.log("FlashCard front :" + firstPresident.front);
+console.log("FlashCard back :" + firstPresident.back);
+
+
+
+
